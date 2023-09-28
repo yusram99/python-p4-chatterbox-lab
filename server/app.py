b760f11c3c74581f7c0fbad49234cc598b668a64
Your Name <youremail@example.com>
2023-09-28 11:11:48 +0300

    commit message

--- server/app.py
+++ server/app.py
@@ -14,13 +14,63 @@ migrate = Migrate(app, db)
 
 db.init_app(app)
 
-@app.route('/messages')
+@app.route('/')
 def messages():
-    return ''
+    return "welcome to chatterbox"
 
-@app.route('/messages/<int:id>')
-def messages_by_id(id):
-    return ''
+@app.route('/messages', methods=['GET'])
+def get_messages():
+    # Retrieve all messages from the database and return them as JSON
+    messages = Message.query.order_by(Message.created_at.asc()).all()
+    return jsonify([message.serialize() for message in messages])
+
+@app.route('/messages/<int:id>', methods=['GET'])
+def get_message_by_id(id):
+    message = Message.query.get(id)
+    if not message:
+        return jsonify({'message': 'Message not found'}), 404
+    return jsonify(message.serialize())
+
+@app.route('/messages', methods=['POST'])
+def create_message():
+    data = request.get_json()
+    new_message = Message(body=data['body'], username=data['username'])
+    db.session.add(new_message)
+    db.session.commit()
+    return jsonify(new_message.serialize()), 201
+
+# @app.route('/messages/<int:id>', methods=['PATCH'])
+# def update_message(id):
+#     message = Message.query.get(id)
+#     if not message:
+#         return jsonify({'message': 'Message not found'}), 404
+#     data = request.get_json()
+#     message.body = data['body']
+#     db.session.commit()
+#     return jsonify(message.serialize())
+@app.route('/messages/<int:id>', methods=['PATCH'])
+def update_message(id):
+    message = Message.query.get(id)
+    if not message:
+        return jsonify({'message': 'Message not found'}), 404
+    
+    data = request.get_json()
+    new_username = data.get('username')  # Get the new username from the request
+    
+    # Update the username if it's provided in the request
+    if new_username is not None:
+        message.username = new_username
+    
+    db.session.commit()
+    return jsonify(message.serialize())
+@app.route('/messages/<int:id>', methods=['DELETE'])
+def delete_message(id):
+    message = Message.query.get(id)
+    if not message:
+        return jsonify({'message': 'Message not found'}), 404
+    db.session.delete(message)
+    db.session.commit()
+    return '', 204
 
 if __name__ == '__main__':
     app.run(port=5555)
--- server/models.py
+++ server/models.py
@@ -1,14 +1,27 @@
+# models.py
+
+from datetime import datetime
 from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy import MetaData
-from sqlalchemy_serializer import SerializerMixin
 
-metadata = MetaData(naming_convention={
-    "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
-})
+db = SQLAlchemy()
 
-db = SQLAlchemy(metadata=metadata)
-
-class Message(db.Model, SerializerMixin):
+class Message(db.Model):
     __tablename__ = 'messages'
 
     id = db.Column(db.Integer, primary_key=True)
+    body = db.Column(db.String, nullable=False)
+    username = db.Column(db.String, nullable=False)
+    created_at = db.Column(db.DateTime, default=datetime.utcnow)
+    updated_at = db.Column(db.DateTime, default=datetime.utcnow, onupdate=datetime.utcnow)
+
+    def __repr__(self):
+        return f'<Message {self.id}>'
+
+    def serialize(self):
+        return {
+            'id': self.id,
+            'body': self.body,
+            'username': self.username,
+            'created_at': self.created_at.isoformat(),
+            'updated_at': self.updated_at.isoformat(),
+        }
